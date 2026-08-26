54f981f907f941b9ff60054fc3455d4d822fb9b6
Amir Batyrov <amir-baturov@mail.ru>
2026-08-26 10:25:14 +0700

    perror on setup failures

--- src/main.cpp
+++ src/main.cpp
@@ -15,15 +15,15 @@ int main() {
   
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
-   std::cerr << "Failed to create server socket\n";
-   return 1;
+    perror("socket");
+    return 1;
   }
   
   // Since the tester restarts your program quite often, setting SO_REUSEADDR
   // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-    std::cerr << "setsockopt failed\n";
+    perror("setsockopt");
     return 1;
   }
   
@@ -33,13 +33,13 @@ int main() {
   server_addr.sin_port = htons(6379);
   
   if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-    std::cerr << "Failed to bind to port 6379\n";
+    perror("bind");
     return 1;
   }
   
   int connection_backlog = 5;
   if (listen(server_fd, connection_backlog) != 0) {
-    std::cerr << "listen failed\n";
+    perror("listen");
     return 1;
   }
   
