e58d794a68631971836b5a6cc5a9abf567dbeb2b
Amir Batyrov <amir-baturov@mail.ru>
2026-09-14 14:42:19 +0700

    unique_fd: invert the destructor guard

--- src/main.cpp
+++ src/main.cpp
@@ -42,10 +42,9 @@ private:
     explicit unique_fd(int fd) : _fd{fd} {}
 
     ~unique_fd() {
-      if (_fd == -1) {
-        return;
+      if (_fd != -1) {
+        close_fd();
       }
-      close_fd();
     }
 
     unique_fd(unique_fd&& other) noexcept : _fd{std::exchange(other._fd, -1)} {}
