227a1670c08a90010fb4c9de814577d8c99811f0
Amir Batyrov <amir-baturov@mail.ru>
2026-09-23 18:21:06 +0700

    serve.sh: drop the template comments

--- serve.sh
+++ serve.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 #
-# Use this script to run your program LOCALLY.
-#
-# Note: Changing this script WILL NOT affect how CodeCrafters runs your program.
-#
-# Learn more: https://codecrafters.io/program-interface
+# Build, gate and run the server: ./serve.sh [args passed to build/redis]
 
 set -e # Exit early if any commands fail
 
 step() { printf '\n\033[1m==> %s\033[0m\n' "$*"; }
 
-# Copied from .codecrafters/compile.sh
-#
-# - Edit this to change how your program compiles locally
-# - Edit .codecrafters/compile.sh to change how your program compiles remotely
 (
   cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
 
@@ -64,8 +56,4 @@ step() { printf '\n\033[1m==> %s\033[0m\n' "$*"; }
 )
 
 step "[6/6] run: build/redis $*"
-# Copied from .codecrafters/run.sh
-#
-# - Edit this to change how your program runs locally
-# - Edit .codecrafters/run.sh to change how your program runs remotely
 exec $(dirname "$0")/build/redis "$@"
